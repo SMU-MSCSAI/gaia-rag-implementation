e4ce76e54e09118c5c2fcb271aad3e4163580dd6
Tango Tew <tang.tew@outlook.com>
2024-07-09 18:44:51 -0500

    install dependencies

---
+++ gaia_framwork/tests/test_sample.py
@@ -0,0 +1,10 @@
+# gaia_framework/tests/test_sample.py
+
+import unittest
+
+class TestSample(unittest.TestCase):
+    def test_example(self):
+        self.assertEqual(1 + 1, 2)
+
+if __name__ == '__main__':
+    unittest.main()
