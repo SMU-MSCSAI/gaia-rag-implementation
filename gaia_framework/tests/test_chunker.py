f84ffca42f685206464b08ca55e0776c5f392739
Tango Tew <tang.tew@outlook.com>
2024-07-09 22:39:21 -0500

    test two overlaps

--- gaia_framework/tests/test_chunker.py
+++ gaia_framework/tests/test_chunker.py
@@ -12,9 +12,17 @@ class TestTextChunker(unittest.TestCase):
             text = "This is a simple text that needs to be chunked into smaller pieces."
             chunker = TextChunker(chunk_size=5, chunk_overlap=1)
             chunks = chunker.chunk_text(text)
-            print(chunks)
             # Check if the number of chunks is correct
+            logger.info(f"Testing the number of chunks: {len(chunks)} with an overlap of 1")
             self.assertEqual(len(chunks), 4) 
+            logger.info(f"TextChunker with overlap of 1: {chunks} is passed")
+            
+            chunker = TextChunker(chunk_size=5, chunk_overlap=2)
+            two_chunks = chunker.chunk_text(text)
+            # Check if the number of chunks is correct
+            logger.info(f"Testing the number of chunks: {len(chunks)} with an overlap of 2")
+            self.assertEqual(len(two_chunks), 5)
+            logger.info(f"TextChunker with overlap of 2: {chunks} is passed")
             # Check if the first chunk contains the expected text
             self.assertIn("This is a simple text", chunks[0])  
             logger.info("TextChunker test passed.")
